edbb44b12faf44fde36ef17d49c339c81aea3d5f
Craig Jellick <craig@rancher.com>
2016-06-30 17:02:12 -0700

    Stubbed out implementation

---
+++ events/listener.go
@@ -0,0 +1,19 @@
+package events
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/rancher/agent/handlers"
+	revents "github.com/rancher/go-machine-service/events"
+)
+
+func Listen(eventURL, accessKey, secretKey string, workerCount int) error {
+	logrus.Infof("Listening for events on %v", eventURL)
+
+	eventHandlers := handlers.GetHandlers()
+	router, err := revents.NewEventRouter("", 0, eventURL, accessKey, secretKey, nil, eventHandlers, "", workerCount)
+	if err != nil {
+		return err
+	}
+	err = router.StartWithoutCreate(nil)
+	return err
+}
---
+++ handlers/common.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"fmt"
+
+	"github.com/Sirupsen/logrus"
+	revents "github.com/rancher/go-machine-service/events"
+	"github.com/rancher/go-rancher/client"
+)
+
+func GetHandlers() map[string]revents.EventHandler {
+	return map[string]revents.EventHandler{
+		"compute.instance.activate":  InstanceActivate,
+		"compute.intance.deactivate": NoOpHandler,
+		"compute.force.stop":         NoOpHandler,
+		"compute.instance.inspect":   NoOpHandler,
+		"compute.instance.pull":      NoOpHandler,
+		"compute.instance.remove":    NoOpHandler,
+		"storage.image.activate":     NoOpHandler,
+		"storage.volume.activate":    NoOpHandler,
+		"storage.volume.deactivate":  NoOpHandler,
+		"storage.volume.remove":      NoOpHandler,
+		"delegate.request":           NoOpHandler,
+		"ping":                       NoOpHandler,
+	}
+}
+
+func reply(replyData map[string]interface{}, event *revents.Event, cli *client.RancherClient) error {
+	if replyData == nil {
+		replyData = make(map[string]interface{})
+	}
+
+	reply := &client.Publish{
+		ResourceId:   event.ResourceID,
+		PreviousIds:  []string{event.ID},
+		ResourceType: event.ResourceType,
+		Name:         event.ReplyTo,
+		Data:         replyData,
+	}
+
+	logrus.Infof("Reply: %+v", reply)
+	err := publishReply(reply, cli)
+	if err != nil {
+		return fmt.Errorf("Error sending reply %v: %v", event.ID, err)
+	}
+	return nil
+}
+
+func publishReply(reply *client.Publish, apiClient *client.RancherClient) error {
+	_, err := apiClient.Publish.Create(reply)
+	return err
+}
