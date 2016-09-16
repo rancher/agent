17f06876c01585f9b552f80b9201f9f2f82d7a50
Daishan Peng <daishan@rancher.com>
2016-09-15 22:47:42 -0700

    remove ping logs

--- handlers/ping.go
+++ handlers/ping.go
@@ -21,7 +21,7 @@ type PingHandler struct {
 }
 
 func (h *PingHandler) Ping(event *revents.Event, cli *client.RancherClient) error {
-	logrus.Infof("Received event: Name: %s, Event Id: %s, Resource Id: %s", event.Name, event.ID, event.ResourceID)
+	logrus.Debugf("Received event: Name: %s, Event Id: %s, Resource Id: %s", event.Name, event.ID, event.ResourceID)
 	if event.Name != "ping" || event.ReplyTo == "" {
 		return nil
 	}
