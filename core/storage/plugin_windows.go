9c501102365e3d82d3a290eccc0ac9ccef4dd9d5
James Oliver <joliver@rancher.com>
2017-09-01 12:00:29 -0700

    get it to compile

--- core/storage/plugin_windows.go
+++ core/storage/plugin_windows.go
@@ -1,5 +1,9 @@
 package storage
 
+import (
+  "github.com/rancher/agent/model"
+)
+
 // callRancherStorageVolumeAttach is not supported on windows
 func callRancherStorageVolumeAttach(volume model.Volume) error {
 	return nil
