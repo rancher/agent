30e380bfaf5656fa7c77238537d769093635c98c
Aiwantaozi <michelia.feng@gmail.com>
2017-09-19 17:19:32 +0800

    Add Aliyun Cloud Provider for agent

---
+++ cloudprovider/aliyun/aliyun_provider.go
@@ -0,0 +1,79 @@
+package aliyun
+
+import (
+	"time"
+
+	"github.com/denverdino/aliyungo/metadata"
+
+	"github.com/rancher/agent/cloudprovider"
+	"github.com/rancher/agent/core/hostInfo"
+)
+
+const (
+	aliyunTag = "aliyun"
+)
+
+type Provider struct {
+	client     metadataClient
+	interval   time.Duration
+	expireTime time.Duration
+}
+
+type metadataClient interface {
+	Region() (string, error)
+	Zone() (string, error)
+}
+
+type metadataClientImpl struct {
+	client *metadata.MetaData
+}
+
+func init() {
+	cloudprovider.AddCloudProvider(aliyunTag, &Provider{
+		expireTime: time.Minute * 3,
+		interval:   time.Second * 5,
+	})
+}
+
+func (m metadataClientImpl) Region() (string, error) {
+	return m.client.Region()
+}
+
+func (m metadataClientImpl) Zone() (string, error) {
+	return m.client.Zone()
+}
+
+func (p *Provider) Init() error {
+	client := metadataClientImpl{metadata.NewMetaData(nil)}
+	p.client = client
+	return nil
+}
+
+func (p *Provider) Name() string {
+	return aliyunTag
+}
+
+func (p *Provider) GetHostInfo() (i *hostInfo.Info, err error) {
+	zone, err := p.client.Zone()
+	if err != nil {
+		return
+	}
+	region, err := p.client.Region()
+	if err != nil {
+		return
+	}
+	i = &hostInfo.Info{}
+	i.Labels = map[string]string{}
+	i.Labels[cloudprovider.RegionLabel] = region
+	i.Labels[cloudprovider.AvailabilityZoneLabel] = zone
+	i.Labels[cloudprovider.CloudProviderLabel] = aliyunTag
+	return
+}
+
+func (p *Provider) ExpireTime() time.Duration {
+	return p.expireTime
+}
+
+func (p *Provider) Interval() time.Duration {
+	return p.interval
+}
