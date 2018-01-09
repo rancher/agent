4f82ef0a255ff8b4b27a67587d3b42a85d1a7cdf
Yuxing <yuxing@rancher.com>
2018-01-09 14:51:26 +0800

    Windows Agent Upgrade Solution
    Related issue https://github.com/rancher/rancher/issues/10662
    Rename ./package/startup.ps1 to ./package/startup_agent.ps1
    The startup.ps1 will become the entrypoint for windows agent startup
    and be placed into rancher/agent-windows

--- package/startup_agent.ps1
+++ package/startup_agent.ps1
@@ -4,16 +4,6 @@ param(
     )
     $strs=$inputStr.Split(",")
     $RegisterUrl=$strs[0].Trim("`"")
-    $HostLabels=$strs[1].Trim("`"")
-    $AgentIp=$strs[2].Trim("`"")
-
-if($HostLabels -ne $null){
-    [System.Environment]::SetEnvironmentVariable("CATTLE_HOST_LABELS",$HostLabels,"Machine")
-}
-if ($AgentIp -ne $null){
-    [System.Environment]::SetEnvironmentVariable("CATTLE_AGENT_IP",$AgentIp,"Machine")
-}
-& 'C:\Program Files\rancher\startup_per-host-subnet.ps1'
 $rancherAgentService=get-service rancher-agent -ErrorAction Ignore
 if($rancherAgentService -ne $null){
     & 'C:\Program Files\rancher\agent.exe' --unregister-service
@@ -21,6 +11,4 @@ if($rancherAgentService -ne $null){
 & 'C:\Program Files\rancher\agent.exe' --register-service $RegisterUrl
 
 start-service rancher-agent
-sleep 5
-start-service rancher-per-host-subnet
 write-host "start agent success"
