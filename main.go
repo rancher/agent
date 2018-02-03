8aa199c34cae2d1b6f321fdd350f93dde11ea519
Darren Shepherd <darren@rancher.com>
2018-02-02 21:55:23 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/rancher/rancher/pkg/remotedialer"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	Token  = "X-API-Tunnel-Token"
+	ID     = "X-API-Tunnel-ID"
+	Params = "X-API-Tunnel-Params"
+)
+
+func main() {
+	if os.Getenv("CATTLE_DEBUG") == "true" || os.Getenv("RANCHER_DEBUG") == "true" {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	token := os.Getenv("CATTLE_TOKEN")
+	params := map[string]interface{}{
+		"customConfig": map[string]string{
+			"address":         os.Getenv("CATTLE_ADDRESS"),
+			"internalAddress": os.Getenv("CATTLE_INTERNAL_ADDRESS"),
+		},
+		"requestedHostname": os.Getenv("CATTLE_NODE_NAME"),
+		"role":              split(os.Getenv("CATTLE_ROLE")),
+	}
+
+	for k, v := range params {
+		if m, ok := v.(map[string]string); ok {
+			for k, v := range m {
+				logrus.Infof("Option %s=%s", k, v)
+			}
+		} else {
+			logrus.Infof("Option %s=%v", k, v)
+		}
+	}
+
+	bytes, err := json.Marshal(params)
+	if err != nil {
+		return err
+	}
+
+	headers := map[string][]string{
+		Token:  {token},
+		Params: {base64.StdEncoding.EncodeToString(bytes)},
+	}
+
+	server := os.Getenv("CATTLE_SERVER")
+	serverURL, err := url.Parse(server)
+	if err != nil {
+		return err
+	}
+
+	wsURL := fmt.Sprintf("wss://%s/v3/connect", serverURL.Host)
+	remotedialer.ClientConnect(wsURL, http.Header(headers), nil, func(proto, address string) bool {
+		switch proto {
+		case "tcp":
+			return true
+		case "unix":
+			return address == "/var/run/docker.sock"
+		}
+		return false
+	})
+
+	return nil
+}
+
+func split(s string) []string {
+	var result []string
+	for _, part := range strings.Split(s, ",") {
+		p := strings.TrimSpace(part)
+		if p != "" {
+			result = append(result, p)
+		}
+	}
+	if len(result) == 1 && result[0] == "" {
+		return nil
+	}
+	return result
+}
---
+++ package/run.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+set -e
+
+error()
+{
+    echo "ERROR:" "$@" 1>&2
+}
+
+drun()
+{
+    docker run -i --rm --net=host $AGENT_IMAGE "$@"
+}
+
+AGENT_IMAGE=${AGENT_IMAGE:-ubuntu:14.04}
+
+export CATTLE_ADDRESS
+export CATTLE_INTERNAL_ADDRESS
+export CATTLE_NODE_NAME
+export CATTLE_ROLE
+export CATTLE_SERVER
+export CATTLE_TOKEN
+
+while true; do
+    case "$1" in
+        -d | --debug)              DEBUG=true                 ;;
+        -s | --server)      shift; CATTLE_SERVER=$1           ;;
+        -t | --token)       shift; CATTLE_TOKEN=$1            ;;
+        -c | --ca-checksum) shift; CA_CHECKSUM=$1             ;;
+        -a | --all-roles)          ALL=true                   ;;
+        -e | --etcd)               ETCD=true                  ;;
+        -w | --worker)             WORKER=true                ;;
+        -p | --controlplane)       CONTROL=true               ;;
+        -n | --node-name)          CATTLE_NODE_NAME=true      ;;
+        --address)          shift; CATTLE_ADDRESS=$1          ;;
+        --internal-address) shift; CATTLE_INTERNAL_ADDRESS=$1 ;;
+        *) break;
+    esac
+    shift
+done
+
+if [ "$DEBUG" = true ]; then
+    set -x
+fi
+
+if [ ! -w /var/run/docker.sock ] || [ ! -S /var/run/docker.sock ]; then
+    error Please bind mount in the docker socket to /var/run/docker.sock
+    error example:  docker run -v /var/run/docker.sock:/var/run/docker.sock ...
+    exit 1
+fi
+
+docker info || {
+    error Failed to run docker
+    exit 1
+}
+
+if [ -z "$CATTLE_NODE_NAME" ]; then
+    CATTLE_NODE_NAME=$(drun hostname -s)
+fi
+
+if [ "$CATTLE_ADDRESS" = "awslocal" ]; then
+    export CATTLE_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+elif [ "$CATTLE_ADDRESS" = "ipify" ]; then
+    export CATTLE_ADDRESS=$(curl -s https://api.ipify.org)
+fi
+
+if [ -z "$CATTLE_ADDRESS" ]; then
+    CATTLE_ADDRESS=$(drun ip route get 8.8.8.8 | grep via | awk '{print $NF}')
+fi
+
+if [ "$ALL" = true ]; then
+    CATTLE_ROLE="etcd,worker,controlplane"
+else
+    if [ "$ETCD" = true ]; then
+        CATTLE_ROLE="${CATTLE_ROLE},etcd"
+    fi
+    if [ "$WORKER" = true ]; then
+        CATTLE_ROLE="${CATTLE_ROLE},worker"
+    fi
+    if [ "$CONTROL" = true ]; then
+        CATTLE_ROLE="${CATTLE_ROLE},controlplane"
+    fi
+fi
+
+if [ -n "$CA_CHECKSUM" ]; then
+    temp=$(mktemp)
+    curl --insecure -s -fL $CATTLE_SERVER/v3/settings/cacerts | jq -r .value > $temp
+    if [ "$(sha256sum $temp | awk '{print $1}')" != $CA_CHECKSUM ]; then
+        rm -f $temp
+        error $CATTLE_SERVER/v3/settings/cacerts does not match $CA_CHECKSUM
+        exit 1
+    fi
+    mkdir -p certs
+    mv $temp certs/serverca
+    export SSL_CERT_DIR=$(pwd)/certs
+fi
+
+if [ -z "$CATTLE_SERVER" ]; then
+    error -- --server is a required option
+    exit 1
+fi
+
+if [ -z "$CATTLE_TOKEN" ]; then
+    error -- --token is a required option
+    exit 1
+fi
+
+if [ -z "$CATTLE_ADDRESS" ]; then
+    error -- --address is a required option
+    exit 1
+fi
+
+exec go run agent.go
